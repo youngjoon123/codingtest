de0fed2e4c961941b44c636c9cf77af2a03db466
youngjoon <junjun0905@naver.com>
2020-05-07 15:30:36 +0900

    테스트

---
+++ cote1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,m,sum=0;
+
+	cin >> n >>m;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i%m == 0)
+		{
+			sum += i;
+		}
+	}
+	cout << sum;
+	return 0;
+
+}
