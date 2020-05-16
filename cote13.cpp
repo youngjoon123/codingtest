466d9b56295b130e73cf666402dda3521454cb65
youngjoon <junjun0905@naver.com>
2020-05-16 16:49:04 +0900

    d

---
+++ cote13.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	
+	int ch[10];
+	int a[100];
+	int max=0, x, res;
+	scanf("&s", a);
+
+	for (int i = 0; a[i] != '\0'; i++) {
+		x = a[i] - 48;
+		ch[x]++;
+	}
+
+	for (int j = 0; j < 10; j++) {
+		if (ch[j] > max) {
+			max = ch[j];
+			res = j;
+		}
+	}
+	printf("%d\n", res);
+	return 0;
+}
